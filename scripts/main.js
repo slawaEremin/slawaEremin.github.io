0ddf7f722b3567aa567c2d92603ba446a0581010
Slawa Eremin <veremin@otalio.com>
2021-01-15 15:45:45 +0300

    update

--- scripts/main.js
+++ scripts/main.js
@@ -1,87 +1,99 @@
-$(function() {
-  // show mobile version
-  var isAndroid = window.navigator.userAgent.match(/Android/i);
-  var isIOS = window.navigator.userAgent.match(/iPhone|iPad|iPod/i);
-  var isMobile = isAndroid || isIOS;
+$(function () {
+    // show mobile version
+    var isAndroid = window.navigator.userAgent.match(/Android/i);
+    var isIOS = window.navigator.userAgent.match(/iPhone|iPad|iPod/i);
+    var isMobile = isAndroid || isIOS;
 
-  if (isMobile) {
-    $('body').addClass('is-mobile');
-  }
+    if (isMobile) {
+        $('body').addClass('is-mobile');
+    }
 
-  // modal
-  var $open = $('[data-open]');
-  var $close = $('[data-close]');
+    // modal
+    var $open = $('[data-open]');
+    var $close = $('[data-close]');
 
-  $open.on('click', function() {
-    $('body').addClass('is-open-contact');
-  });
+    $open.on('click', function () {
+        $('body').addClass('is-open-modal');
+    });
 
-  $close.on('click', function() {
-    $('body').removeClass('is-open-contact');
-  });
+    $close.on('click', function () {
+        $('body').removeClass('is-open-modal');
+    });
 
-  //modal demo
-  var $demoOpen = $('[data-demo-open]');
-  var $demoClose = $('[data-demo-close]');
+    //modal demo
+    var $demoOpen = $('[data-demo-open]');
+    var $demoClose = $('[data-demo-close]');
 
-  $demoOpen.on('click', function() {
-    $('body').addClass('is-open-demo');
-  });
+    $demoOpen.on('click', function () {
+        $('body').addClass('is-open-demo');
+    });
 
-  $demoClose.on('click', function() {
-    $('body').removeClass('is-open-demo');
-  });
+    $demoClose.on('click', function () {
+        $('body').removeClass('is-open-demo');
+    });
 
-  //menu
-  var $menu = $('[data-menu-root]');
-  var $menuOpen = $('[data-menu-open]');
-  var $menuClose = $('[data-menu-close]');
+    //contacts
+    var $contactOpen = $('[data-contact-open]');
+    var $contactClose = $('[data-contact-close]');
 
-  $menuOpen.on('click', function() {
-    $menu.addClass('is-mobile-open');
-  });
+    $contactOpen.on('click', function () {
+        $('body').addClass('is-open-contact');
+    });
 
-  $menuClose.on('click', function() {
-    $menu.removeClass('is-mobile-open');
-  });
+    $contactClose.on('click', function () {
+        $('body').removeClass('is-open-contact');
+    });
 
-  //menu
-  $(document).scroll(function () {
-    var $nav = $("[data-menu-root]");
-    $nav.toggleClass('type-active', $(this).scrollTop() > $nav.height());
-  });
+    //menu
+    var $menu = $('[data-menu-root]');
+    var $menuOpen = $('[data-menu-open]');
+    var $menuClose = $('[data-menu-close]');
+
+    $menuOpen.on('click', function () {
+        $menu.addClass('is-mobile-open');
+    });
+
+    $menuClose.on('click', function () {
+        $menu.removeClass('is-mobile-open');
+    });
+
+    //menu
+    $(document).scroll(function () {
+        var $nav = $("[data-menu-root]");
+        $nav.toggleClass('type-active', $(this).scrollTop() > $nav.height());
+    });
 })
 
 $(document).ready(function () {
 
-  if (document.getElementById('redoc-container')) {
-    (function() {
-      if (typeof Redoc !== 'undefined') {
-        Redoc.init('/json/api-public.json', {
-          theme: {
-            breakpoints: {
-              medium: '200rem'
+    if (document.getElementById('redoc-container')) {
+        (function () {
+            if (typeof Redoc !== 'undefined') {
+                Redoc.init('/json/api-public.json', {
+                    theme: {
+                        breakpoints: {
+                            medium: '200rem'
+                        }
+                    },
+                    nativeScrollbars: true,
+                    scrollYOffset: 100
+                }, document.getElementById('redoc-container'), () => {
+                    $('div[data-id]').each(function () {
+                        $('#' + $(this).data('id')).append($(this));
+                        $(this).removeClass('d-none');
+                    })
+                    var pre = $('.code-block pre'),
+                        pl = pre.length;
+                    for (var i = 0; i < pl; i++) {
+                        pre[i].innerHTML = '<span class="line-number"></span>' + pre[i].innerHTML + '<span class="cl"></span>';
+                        var num = pre[i].innerHTML.split(/\n/).length;
+                        for (var j = 0; j < num; j++) {
+                            var line_num = pre[i].getElementsByTagName('span')[0];
+                            line_num.innerHTML += '<span>' + (j + 1) + '</span>';
+                        }
+                    }
+                });
             }
-          },
-          nativeScrollbars: true,
-          scrollYOffset: 100
-        }, document.getElementById('redoc-container'), () => {
-          $('div[data-id]').each(function(){
-            $('#' + $(this).data('id')).append($(this));
-            $(this).removeClass('d-none');
-          })
-          var pre = $('.code-block pre'),
-              pl = pre.length;
-          for (var i = 0; i < pl; i++) {
-            pre[i].innerHTML = '<span class="line-number"></span>' + pre[i].innerHTML + '<span class="cl"></span>';
-            var num = pre[i].innerHTML.split(/\n/).length;
-            for (var j = 0; j < num; j++) {
-              var line_num = pre[i].getElementsByTagName('span')[0];
-              line_num.innerHTML += '<span>' + (j + 1) + '</span>';
-            }
-          }
-        });
-      }
-    })();
-  }
+        })();
+    }
 });
